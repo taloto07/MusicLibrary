6d372c6c26a645e1a0953aa0823db7da28ac16bf
taloto07 <taloto07@gmail.com>
2013-12-31 00:20:29 -0800

    Revert "Revert d63731d..8126fba"
    This reverts commit ffc1b40a6037ddef4da047ae2d6f81d549c84fa3.

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ *///173.254.28.56
+    $db_hostname = "just56.justhost.com";
+    $db_database = "koolninc_kool9";
+    $db_username = 'koolninc_H';
+    $db_password = 'Abc123456';
+    
+    
+?>
