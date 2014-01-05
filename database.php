ce951005df7f216c4d4ad2b99eace77a0843249d
taloto07 <taloto07@gmail.com>
2014-01-04 20:15:49 -0800

    testing only

--- database.php
+++ database.php
@@ -5,9 +5,29 @@
  * and open the template in the editor.
  *///173.254.28.56
     $db_hostname = "just56.justhost.com";
-    $db_database = "koolninc_kool9";
+    $db_database = "koolninc_music";
     $db_username = 'koolninc_H';
     $db_password = 'Abc123456';
     
-    
+    try{
+        $conn = new PDO("mysql:host=$db_hostname;dbname=$db_database", $db_username, $db_password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->query("SET NAMES UTF8");
+
+        $rows = $conn->query("SELECT * FROM songs");
+
+        if (count($rows)){
+            foreach ($rows as $row){
+                echo "<div id='playMusic' class='playMusic' value='$row[2]'>$row[0] $row[1]</div>";
+            }
+        }else{
+            echo "No result.";
+        }
+
+        // Close connection
+        $conn = null;
+    }catch(PDOException $e){
+        echo "ERROR: ". $e->getMessage();
+    }
 ?>
+
